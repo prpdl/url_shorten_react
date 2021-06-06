8e58f973237c41ba3112716e9e1ac3c98f2ed3a4
prpdl <prp_slayer@hotmail.com>
2021-06-06 19:15:20 +1000

    Banner text changed to extra bold

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,23 @@
 import React from 'react'
 const Banner = ({image}) => {
     return (
-        <div className="banner flex flex-wrap">
-           <img className="min-w-full max-h-96 object-cover" src={image}/>
-           <div className="banner-text absolute left-20 top-24 sm:top-40 sm:left-40 text-lg sm:text-2xl md:text-3xl lg:text-4xl font-mono text-yellow-600 flex-shrink">
-           <p>Have Full Control Over Your Links</p>
-           <p>Paste The Link Below To Shortern It</p>
+        <div className="banner flex flex-wrap relative">
+           <img className="min-w-full h-50v object-cover" src={image}/>
+           <div className="banner-text absolute text-lg sm:text-2xl md:text-3xl lg:text-4xl font-mono font-extrabold text-yellow-600">
+           <p className="p-4">Have Full Control Over Your Links</p>
+           <p className="p-4">Paste The Link Below To Shortern It</p>
            </div>
-           
         </div>
     )
 }
 
 export default Banner
+{/* Things to do:
+    1. Make the navigation button responsive
+    2. Add table showing top visited URLs.
+    3. Add User Auth
+    Optional:
+    4. Non signed users can shorten the link. 
+    5. Signed Users get the list of their Shortened URLS.
+    6. Signed Users can cusom create their Short URLs
+*/}
