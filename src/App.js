5b1fecd56ab2e9dd20640ea3ff7e2bcd0b346d8e
prpdl <prp_slayer@hotmail.com>
2021-06-06 19:19:53 +1000

    Component Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 import './App.css';
 import SearchBar from './components/SearchBar';
@@ -7,43 +7,48 @@ import Navigation from './components/Navigation';
 
 import bannerImage from './images/banner/tech_b_01.png'
 import Shorten from './components/Shorten';
+import Testarea from './components/Testarea';
+import Footer from './components/Footer';
 
 
 function App() {
 
- 
-  const[shortUrl, setShortUrl] = useState('')
+
+  const [shortUrl, setShortUrl] = useState('')
 
   const shrink = (fullUrl) => {
-     console.log('Sending Data...')
-     const post = async () => {
-         console.log('here')
-         try {
-             const result = await axios.post('http://localhost:3000/url/', 
-          {fullUrl});
+    console.log('Sending Data...')
+    const post = async () => {
+      try {
+        const result = await axios.post('http://localhost:3000/api/url/',
+          { fullUrl });
 
-          console.log(result)
-          if(result.data.message) {
-              setShortUrl(result.data.getUrl.short)
-              console.log(result.data.getUrl.short)
-          }else{
-              setShortUrl(result.data.newRecord.short)
-              console.log(result.data.newRecord.short)
-          }
-         }catch(error) {
-             console.log(error)
-         }
-     }
+        console.log(result)
+        if (result.data.message) {
+          setShortUrl(result.data.url.short)
+          console.log(result.data.url.short)
+        } else {
+          setShortUrl(result.data.newRecord.short)
+          console.log(result.data.newRecord.short)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
-     post();
+    post();
   }
 
   return (
     <div className="app">
-      <Navigation/>
-      <Banner image={bannerImage}/>
-      <SearchBar handleClick={shrink}/>
-      <Shorten shortUrl={shortUrl}/>
+      <Navigation />
+      <Banner image={bannerImage} />
+      <div className="bg-gray-100 mt-5">
+        <SearchBar handleClick={shrink} setShortUrl={setShortUrl} />
+        <Shorten shortUrl={shortUrl} />
+      </div>
+      <Footer />
+      {/* <Testarea/> */}
     </div>
   );
 }
